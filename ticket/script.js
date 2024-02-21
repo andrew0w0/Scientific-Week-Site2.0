4f7e7316ca58615196d6c51c469583a13bf0c789
andrew0w0 <90263133+andrew0w0@users.noreply.github.com>
2024-02-21 08:16:08 +0000

    Added personalize function

--- ticket/script.js
+++ ticket/script.js
@@ -1,7 +1,7 @@
 const div = document.getElementById('schoolclass');
 const a = document.getElementById('schoolclassa');
 
-if(window.innerWidth < 920) {
+if (window.innerWidth < 920) {
   div.style.display = 'none';
   a.style.display = 'none';
 } else {
@@ -18,7 +18,7 @@ elementh1.innerHTML = text.join(' ');
 const elementp = document.getElementById('attention')
 const text1 = elementp.innerHTML.split(' ');
 
-text1[0] ='<span class="red">' + text1[0] + '</span>';
+text1[0] = '<span class="red">' + text1[0] + '</span>';
 elementp.innerHTML = text1.join(' ');
 
 function reveal() {
@@ -39,17 +39,44 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
-window.addEventListener("resize", function () {
+window.addEventListener("resize", function() {
   const div = document.getElementById('schoolclass');
   const a = document.getElementById('schoolclassa');
 
-  if(window.innerWidth < 920) {
+  if (window.innerWidth < 920) {
     div.style.display = 'none';
     a.style.display = 'none';
   } else {
     div.style.display = 'flex';
     a.style.display = 'flex';
   }
+})
+
+const createButton = document.getElementById('ticket-button');
+const componentsDiv = document.getElementById('components');
+
+createButton.addEventListener('click', (Event) => {
+
+  const inputDiv = document.createElement("div");
+  inputDiv.className = "inputs-ticket"
+
+  const input1 = document.createElement("input");
+  input1.id = 'input-name'
+  input1.placeholder = 'Come ti chiami?'
+  const input2 = document.createElement("input");
+  input2.id = 'input-location'
+  input2.placeholder = "Di dove sei?"
+
+  const confirmButton = document.createElement("button");
+  confirmButton.id = "confirm-button"
+  confirmButton.textContent = "Crea il tuo biglietto"
+
+  componentsDiv.removeChild(createButton);
+
+  inputDiv.appendChild(input1);
+  inputDiv.appendChild(input2)
+  componentsDiv.appendChild(inputDiv);
+  componentsDiv.appendChild(confirmButton)
 })
 
 
